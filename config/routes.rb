697d9c3fac50f1076ae9714a975c6856430f8ab3
Matheus Cunha <mathcunha@gmail.com>
2020-01-02 20:11:22 +0000

    update root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   resources :portfolios
-  get 'pages/home'
+    
   get 'about-me', to:'pages#about'
   get 'pages/contact'
   
   resources :blogs
+
+  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
