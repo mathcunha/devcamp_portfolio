2aa592a2cbeba471869395bf42cc6c86c908ec88
Matheus Cunha <mathcunha@gmail.com>
2020-01-06 15:39:39 +0000

    data defaults using model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,10 +1,18 @@
 class Portfolio < ApplicationRecord
 
-    validates_presence_of :title, :subtitle, :body
+  validates_presence_of :title, :subtitle, :body
 
-    def self.angular
-        where.(subtitle: 'angular')
-    end
+  def self.angular
+    where.(subtitle: 'angular')
+  end
 
-    scope :dathomirian, -> {where(subtitle:'Dathomirian')}
+  scope :dathomirian, -> {where(subtitle:'Dathomirian')}
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    # shortcut for saying if self.main_image is nil then set 
+    self.main_image ||= "http://placehold.it/600x400"
+    self.thumb_image ||= "http://placehold.it/350x200"
+  end
 end
