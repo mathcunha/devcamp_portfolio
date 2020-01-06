9eea7c4951343baf49714d6a1da846ed8d1a2839
Matheus Cunha <mathcunha@gmail.com>
2020-01-06 18:59:51 +0000

    Integrated technology with portfolio|rails g model Technology name:string portfolio:references

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,39 +7,46 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 5.times do
-    Topic.create(
-        title: Faker::Games::Heroes.specialty
-    )
+  Topic.create(
+    title: Faker::Games::Heroes.specialty
+  )
 end
 
 10.times do 
-    Blog.create(
-        title: Faker::TvShows::Seinfeld.character,
-        body: Faker::Hipster.paragraph,
-        topic_id: Faker::Number.between(from: 1, to: 3),
-        status: Faker::Number.between(from: 0, to: 1)
-    )
+  Blog.create(
+    title: Faker::TvShows::Seinfeld.character,
+    body: Faker::Hipster.paragraph,
+    topic_id: Faker::Number.between(from: 1, to: 3),
+    status: Faker::Number.between(from: 0, to: 1)
+  )
 end
 
 3.times do
-    Topic.create(
-        title: Faker::Games::Heroes.specialty
-    )
+  Topic.create(
+    title: Faker::Games::Heroes.specialty
+  )
 end
 
 5.times do
-    Skill.create(
-        title: Faker::Games::Heroes.specialty,
-        percent_utilized: Faker::Number.decimal_part(digits: 2)
-    )
+  Skill.create(
+    title: Faker::Games::Heroes.specialty,
+    percent_utilized: Faker::Number.decimal_part(digits: 2)
+  )
 end
 
 5.times do
-    Portfolio.create(
-        title: Faker::Movies::StarWars.character,
-        subtitle: Faker::Movies::StarWars.specie,
-        body: Faker::Movies::StarWars.quote,
-        main_image: "http://placehold.it/600x400",
-        thumb_image: "http://placehold.it/350x200"
-    )
+  Portfolio.create(
+    title: Faker::Movies::StarWars.character,
+    subtitle: Faker::Movies::StarWars.specie,
+    body: Faker::Movies::StarWars.quote,
+    main_image: "http://placehold.it/600x400",
+    thumb_image: "http://placehold.it/350x200"
+  )
+end
+
+6.times do
+  Technology.create(
+    name: Faker::ProgrammingLanguage.name,
+    portfolio_id: Faker::Number.between(from: 0, to: 1)
+  )
 end
