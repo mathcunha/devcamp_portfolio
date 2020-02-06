265c2a0acebbe5352dd161737b0e5e3b24970b83
Matheus Cunha <mathcunha@gmail.com>
2020-02-06 19:54:33 +0000

    implemented create action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,20 @@
 class CommentsController < ApplicationController
+  load_and_authorize_resource
   
+  def create
+    @comment = current_user.comments.build(comment_params)    
+    respond_to do |format|
+      if @comment.save
+        format.json { render json: @comment, status: :ok}
+      else
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end    
+  end
+
+  private
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def comment_params
+      params.require(:comment).permit(:content, :blog_id)
+    end
 end
